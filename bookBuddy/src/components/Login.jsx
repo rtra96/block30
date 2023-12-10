c48d8bf687b56e4b8588c7a6c59221d83b4bd60a
Rick Trahant <rtra96@gmail.com>
2023-12-10 15:29:37 -0600

    login page added

--- bookBuddy/src/components/Login.jsx
+++ bookBuddy/src/components/Login.jsx
@@ -1,9 +1,69 @@
-/* TODO - add your code to create a functional React component that renders a login form */
-import React from "react";
+import React, { useState } from 'react';
 
-const Login = () => {
-    return <div>Login form goes here</div>;
+const LoginForm = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
 
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/users/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        const responseData = await response.json();
+        console.log('Login Successful!:', responseData);
+    } else {
+        console.error('Login failed:', response.statusText);
+    }
+    } catch (error) {
+      console.error('Login failed:', error.message);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login to an Existing Account</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Email:
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+
+        <label>
+          Password:
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 };
 
-export default Login;
+export default LoginForm;
